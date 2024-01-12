6817077419991fce71fc42fdfc47f2aa29cf0938
buncram <cramium@kosagi.com>
2024-01-12 14:15:02 +0800

    fix verify address generation

--- jtag_tool.py
+++ jtag_tool.py
@@ -713,7 +713,7 @@ class TexWriter():
         self.write_cmd("RW JTAG-REG, IR={6'h09,2'b10}(addr:('h09)), DR data:(40'h0000000414)")
         # write addresses
         y_address = (address & 0b11_1110_0000) >> 5
-        self.write_comment(f"User inputs Write Y address = 0x{y_address:x} (DR bit[23:16])")
+        self.write_comment(f"User inputs WRITE Y address = 0x{y_address:x} (DR bit[23:16])")
         self.write_cmd(f"RW JTAG-REG, IR={{6'h04,2'b10}}(addr:('h04)), DR data:(40'h{(y_address << 16) | 3:010x})")
         self.write_comment("User issues BIST LOAD command and, starts bist_run")
         self.write_cmd("RW JTAG-REG, IR={6'h06,2'b10}(addr:('h06)), DR data:(40'h0000602c80)")
@@ -734,6 +734,8 @@ class TexWriter():
         self.write_cmd("RW JTAG-REG, IR={6'h0b,2'b10}(addr:('h0b)), DR data:(40'h000000000a), expected DR data:(40'h000000000a)")
 
     def verify_rram(self, address, checkdata, bank):
+        x_address = (address & 0b11_1111_1111_1100_0000_0000) >> 10
+        y_address = (address & 0b11_1110_0000) >> 5
         self.set_bank(bank)
         self.write_testname(f'Verify single word w/ECC ON at address 0x{address:x}')
         self.write_cmd("RW JTAG-REG, IR={6'h03,2'b10}(addr:('h03)), DR data:(40'h0000000000)")
@@ -753,8 +755,9 @@ class TexWriter():
         self.write_comment("select main array/INFO/redundancy/INFO1 area for write")
         self.write_cmd("RW JTAG-REG, IR={6'h07,2'b10}(addr:('h07)), DR data:(40'h0000000002)")
         # read addresses
-        self.write_comment(f"User inputs read address = 0x{address:x} (DR bit[23:0])")
-        self.write_cmd(f"RW JTAG-REG, IR={{6'h04,2'b10}}(addr:('h04)), DR data:(40'h{address:010x})")
+        bank_address = (x_address & 0xFFFF) | ((y_address & 0xFF) << 16)
+        self.write_comment(f"User inputs READ address = 0x{bank_address:x} (X address=0x{x_address:x}, Y address=0x{y_address:x}) (DR bit[23:0])")
+        self.write_cmd(f"RW JTAG-REG, IR={{6'h04,2'b10}}(addr:('h04)), DR data:(40'h{bank_address:010x})")
         # issue read command
         self.write_comment(f"User inputs READ # of loop=0x0")
         self.write_cmd(f"RW JTAG-REG, IR={{6'h10,2'b10}}(addr:('h10)), DR data:(40'h0000000000)")
