b577ad047c2235c3c21913a52a6c50a993425aa2
bunnie <cramium@kosagi.com>
2024-04-28 09:10:26 -0900

    add more helper scripts

---
+++ kernel.sh
@@ -0,0 +1,1 @@
+sudo python ./rram_burn.py -f xous.bin --exec --xous-region kernel --no-verify
--- reset.sh
+++ reset.sh
@@ -9,8 +9,11 @@ echo 0 > /sys/class/gpio/gpio21/value
 
 sudo killall openocd
 sudo openocd -f jlink.cfg -f cramsoc.cfg &
-sleep 0.5
+sleep 1.0
 
 echo 1 > /sys/class/gpio/gpio21/value
-
+sleep 0.5
+echo 0 > /sys/class/gpio/gpio21/value
+sleep 0.5
+echo 1 > /sys/class/gpio/gpio21/value
 # riscv-none-elf-gdb -ex "target extended-remote :3333" -ex "mon reset" -ex "continue"
